6e8094140b1efd4654ce000339603f703d7b2245
Khaled Abdulaziz <khaled.ibrahim.user@gmail.com>
2022-08-23 23:36:36 +0200

    update header file

--- Application/app.h
+++ Application/app.h
@@ -3,10 +3,6 @@
 #define APP_H
 
 #include "../Server/server.h"
-#define MAX_A_DB 255
-#define MAX_T_DB 255
-extern ST_accountsDB_t Accounts_DB[MAX_A_DB];
-extern ST_transaction_t Transactions_DB[MAX_T_DB];
 
 void appStart(void);
 
