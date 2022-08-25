ae20bda379b9f0cba1a888e72c96874294324a82
Khaled Abdulaziz <khaled.ibrahim.user@gmail.com>
2022-08-25 02:41:08 +0200

    add more comments

--- Application/app.c
+++ Application/app.c
@@ -9,22 +9,28 @@ int main(void)
 
 void appStart(void)
 {
-    ST_transaction_t transData;
-    getCardHolderName(&transData.cardHolderData);
-    getCardExpiryDate(&transData.cardHolderData);
-    getCardPAN(&transData.cardHolderData);
-    getTransactionDate(&transData.terminalData);
-    if (isCardExpired(&transData.cardHolderData, &transData.terminalData))
+    ST_transaction_t transData;                   /* creating a struct of type ST_transaction_t */
+    getCardHolderName(&transData.cardHolderData); /* calling of a function to get the name from the user */
+    getCardExpiryDate(&transData.cardHolderData); /* calling of a function to get the expiration date from the user */
+    getCardPAN(&transData.cardHolderData);        /* calling of a function to get the PAN from the user */
+    getTransactionDate(&transData.terminalData);  /* calling of a function to get the transaction date from the user */
+
+    /* check if the card is expired by calling the function to compare both expiration and transaction date */
+    if (isCardExpired(&transData.cardHolderData, &transData.terminalData)) 
     {
         printf("DECLINED_EXPIRED_CARD\n");
         return;
     }
-    getTransactionAmount(&transData.terminalData);
-    setMaxAmount(&transData.terminalData);
+
+    getTransactionAmount(&transData.terminalData); /* calling of a function to get the transaction amount from the user */
+    setMaxAmount(&transData.terminalData);         /* calling of a function to set the maximum amount */
+
+    /* check if the amount exceeding the limit by calling the function to compare both amount and maximum */
     if (isBelowMaxAmount(&transData.terminalData))
     {
         printf("DECLINED_AMOUNT_EXCEEDING_LIMIT\n");
         return;
     }
-    receiveTransactionData(&transData);
+	
+    receiveTransactionData(&transData); /* calling function to check validity status, balance and saving of transaction */
 }
